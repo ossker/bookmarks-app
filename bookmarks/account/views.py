e8f86e29b75a59e164b08b61ea7bb52a09e7ab00
ossker <oskar.bienko@onet.pl>
2023-03-12 17:43:36 +0100

    Login and logout view created. Urls added.

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,3 +1,24 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import LoginForm
 
-# Create your views here.
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password']) # sprawdza dane i zwraca User
+            if user is not None:
+                if user.is_active:
+                    login(request, user) # umieszcza w sesji
+                    return HttpResponse('Uwierzytelnienie zakończyło się sukcesem.')
+                else:
+                    return HttpResponse('Konto jest zablokowane.')
+            else:
+                return HttpResponse('Nieprawidłowe dane.')
+
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
