4f1d1f20c4dc04f3b94247bd8e4ccec1c5c6937a
ossker <oskar.bienko@onet.pl>
2023-03-12 16:07:54 +0100

    Django project and app created.

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\oskar\Desktop\Projekty Django\bookmarks\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
