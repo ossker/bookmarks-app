0ea2c85503fc61c819a8a3564fecf3add85010a9
ossker <oskar.bienko@onet.pl>
2023-03-30 00:40:11 +0200

    Query optimized for retrieving user actions and related objects.

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -67,7 +67,7 @@ def dashboard(request):
     following_ids = request.user.following.values_list('id', flat=True)
     if following_ids:
         actions = actions.filter(user_id__in=following_ids)
-    actions = actions[:10]
+    actions = actions.select_related('user', 'user__profile')[:10].prefetch_related('target')[:10]
     return render(request, 'account/dashboard.html', {'section': 'dashboard', 'actions': actions})
 
 @login_required
